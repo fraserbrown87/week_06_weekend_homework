6e6b3208e5ebdeb8a739828c7c27a1da4c953008
fraserbrown87 <fraserbrown87@gmail.com>
2017-12-09 23:03:04 +0000

    Conference Room tests passed

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -2,8 +2,16 @@ public class ConferenceRoom extends Room {
 
     private String roomName;
 
-    public ConferenceRoom(int capacity) {
+    public ConferenceRoom(int capacity, String roomName) {
         super(capacity);
+        this.roomName = roomName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
         this.roomName = roomName;
     }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
+
+    ConferenceRoom conferenceroom;
+
+    @Before
+    public void before(){
+        conferenceroom = new ConferenceRoom(100, "Ballers Room");
+    }
+
+    @Test
+    public void getRoomName(){
+        assertEquals("Ballers Room", conferenceroom.getRoomName());
+    }
+
+    @Test
+    public void setRoomName(){
+        conferenceroom.setRoomName("Scientology Room");
+        assertEquals("Scientology Room", conferenceroom.getRoomName());
+    }
+
 }
